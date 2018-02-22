efd195485a8a57e2d66c4711dfa5c10afabf5901
Julio Hurtado <juliens@outlook.com>
2018-02-22 07:39:39 -0500

    Update settings.py

--- hlinformation/settings.py
+++ hlinformation/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$#@&!vi!=$!lw=bcv)dife$iek#=qx*fp6%9r7s#*m4y_59u9*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
